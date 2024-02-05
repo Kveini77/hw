b94bbeabb8c85a95ebdfeeaba882757bdbac2ad1
Kveini77 <kveini77@gmail.com>
2024-02-05 21:00:21 +0600

    homework

---
+++ hero.py
@@ -0,0 +1,24 @@
+
+class SuperHero:
+    people = "people"
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase):
+        self.name = name
+        self.nickname = nickname
+        self.superpower = superpower
+        self.health_points = health_points
+        self.catchphrase = catchphrase
+
+    def double_health(self):
+        self.health_points *= 2
+
+    def __str__(self):
+        return (f'Прозвище героя: {self.nickname} \n'
+                f'Суперспособность героя:{self.superpower} \n'
+                f'Здоровье героя: {self.health_points} \n')
+    def __len__(self):
+        return len(self.catchphrase)
+
+hero = SuperHero("Bruh", "Рен Довер", "Щит оправдания", 100, "Бывает")
+print(hero)
+print(len(hero))
